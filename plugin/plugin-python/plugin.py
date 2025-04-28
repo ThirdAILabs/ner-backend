1090f47b786e4cd19f0f8906bfadbd36bd78e03b
Kartik <kartik@thirdai.com>
2025-04-28 21:01:11 +0000

    custom python models

--- plugin/plugin-python/plugin.py
+++ plugin/plugin-python/plugin.py
@@ -1,6 +1,9 @@
 from concurrent import futures
 import sys
 import time
+import json
+from typing import Dict
+from models import Model
 
 import grpc
 
@@ -10,21 +13,40 @@ from proto import model_pb2_grpc
 from grpc_health.v1.health import HealthServicer
 from grpc_health.v1 import health_pb2, health_pb2_grpc
 
+from models import CombinedNERModel
+
+import argparse
+
+model_dict: Dict[str, Model] = {
+    "python_combined_ner_model": CombinedNERModel
+}
+
 class ModelServicer(model_pb2_grpc.ModelServicer):
     """Implementation of Model service."""
 
+    def __init__(self, model):
+        self.model = model
+
     def Predict(self, request, context):
         sentence = request.sentence
-        result = model_pb2.PredictResponse(entities=[{"label": "label", "text": "text", "start": 0, "end": 4}]*5000)
+        preds = self.model.predict(sentence).to_go()
+        result = model_pb2.PredictResponse(entities=preds)
         return result
 
-def serve():
+def serve(model_name: str, **kwargs):
     health = HealthServicer()
     health.set("plugin", health_pb2.HealthCheckResponse.ServingStatus.Value('SERVING'))
 
+    # Create the model instance
+    model_class = model_dict.get(model_name)
+    if model_class is None:
+        raise ValueError(f"Model {model_name} not found in model_dict.")
+    
+    model = model_class(**kwargs)
+
     # Start the server.
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    model_pb2_grpc.add_ModelServicer_to_server(ModelServicer(), server)
+    model_pb2_grpc.add_ModelServicer_to_server(ModelServicer(model), server)
     health_pb2_grpc.add_HealthServicer_to_server(health, server)
     port = server.add_insecure_port('127.0.0.1:0') 
     server.start()
@@ -44,4 +66,11 @@ def serve():
         raise
 
 if __name__ == '__main__':
-    serve()
+    parser = argparse.ArgumentParser(description="Start the NER model gRPC server.")
+    parser.add_argument('--model-name', type=str, required=True, help="Name of the model to serve.")
+    parser.add_argument('--kwargs', type=str, default="{}", help="Additional keyword arguments for the model in JSON format.")
+    args = parser.parse_args()
+
+    kwargs = json.loads(args.kwargs)
+
+    serve(model_name=args.model_name, **kwargs)
