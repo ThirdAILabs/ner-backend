b463edd2ddaba6f8d49b1d87d49a91a87b12f6e1
Mr-Ye-Cao <yecaoyc2019@gmail.com>
2025-05-07 16:31:10 -0500

    basic electron app

---
+++ electron-ner-app/main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object to prevent it from being garbage collected
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Load the index.html file
+  mainWindow.loadFile('renderer/index.html');
+
+  // Open DevTools in development
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed
+  mainWindow.on('closed', () => {
+    // Dereference the window object
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window when the dock icon is clicked
+    if (mainWindow === null) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+}); 
