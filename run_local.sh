1ad5b84e3dfe9480f8239435203e211df77341ad
BenGeordie <school@bengeordie@gmail.com>
2025-05-08 05:36:39 -0500

    Fix convenience scripts

--- run_local.sh
+++ run_local.sh
@@ -1,8 +1,10 @@
 FILE_DIR=$(dirname "$0")
 
 cd "$FILE_DIR/frontend"
-npm i
+npm i --legacy-peer-deps
 npm run build
+rm -rf ../cmd/local/out
+mv out/ ../cmd/local/out/
 
-cd "$FILE_DIR"
+cd ..
 go run cmd/local/main.go
