550ee78575ea84fe51212a768b6a652300f1a33e
Kartik Sarangmath <kartik@thirdai.com>
2025-05-14 16:20:20 -0500

    tests

--- internal/licensing/free_license.go
+++ internal/licensing/free_license.go
@@ -15,21 +15,31 @@ const DefaultFreeLicenseMaxBytes = 5 * 1024 * 1024 * 1024 // 5 GB
 type FreeLicenseVerifier struct {
 	db       *gorm.DB
 	maxBytes int
+	timeNow  func() time.Time
 }
 
 func NewFreeLicenseVerifier(db *gorm.DB, maxBytes int) *FreeLicenseVerifier {
-	return &FreeLicenseVerifier{db: db, maxBytes: maxBytes}
+	return &FreeLicenseVerifier{
+		db:       db,
+		maxBytes: maxBytes,
+		timeNow:  time.Now,
+	}
+}
+
+func (verifier *FreeLicenseVerifier) SetTimeNow(timeNow func() time.Time) {
+	verifier.timeNow = timeNow
 }
 
 func (verifier *FreeLicenseVerifier) VerifyLicense(ctx context.Context) error {
 	var totalBytes sql.NullInt64
-	// Get first day of current month
-	now := time.Now().UTC()
+
+	// Get first day of current month, we apply the quota on a monthly basis
+	now := verifier.timeNow().UTC()
 	currentMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
 
 	if err := verifier.db.WithContext(ctx).Model(&database.InferenceTask{}).
 		Select("SUM(total_size)").
-		Where("start_time >= ?", currentMonth).
+		Where("creation_time >= ?", currentMonth).
 		Scan(&totalBytes).Error; err != nil {
 		slog.Error("error getting total usage", "error", err)
 		return ErrLicenseVerificationFailed
