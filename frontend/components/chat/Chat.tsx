8a7398a157c2ef095a8c26dde63d8595f5444029
Anand Kumar <anand@thirdai.com>
2025-05-22 11:52:19 +0530

    nits

--- frontend/components/chat/Chat.tsx
+++ frontend/components/chat/Chat.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Send, MessageSquare } from 'lucide-react';
+import { HiChip } from "react-icons/hi";
 
 export interface Message {
   id: string;
@@ -17,22 +18,22 @@ interface ChatInterfaceProps {
 const ChatInterface: React.FC<ChatInterfaceProps> = ({
   jobId,
   onSendMessage,
-      messages = [{
-          id: "m-2",
-          content: `What is Lorem Ipsum?Lorem Ipsum is What is Lorem Ipsum?
+  messages = [{
+    id: "m-2",
+    content: `What is Lorem Ipsum?Lorem Ipsum is What is Lorem Ipsum?
   pularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-          role: "user",
-      }, {
-          id: "m-1",
-          content: `What is Lorem Ipsum?Lorem Ipsum is What is Lorem Ipsum?
+    role: "user",
+  }, {
+    id: "m-1",
+    content: `What is Lorem Ipsum?Lorem Ipsum is What is Lorem Ipsum?
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
           Why do we use it?
           e so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-          role: "llm",
-      }],
+    role: "llm",
+  }],
   isLoading = false,
-//   messages = [],
+  //   messages = [],
 }) => {
   const [inputMessage, setInputMessage] = useState('');
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -88,11 +89,10 @@ const ChatInterface: React.FC<ChatInterfaceProps> = ({
               className={`flex ${message.role === 'user' ? 'justify-end' : 'justify-start'}`}
             >
               <div
-                className={`max-w-${message.role === 'user' ? '[70%]' : '[100%]'} rounded-xl p-3 ${
-                  message.role === 'user'
-                    ? 'bg-gray-100 text-gray-700 p-6'
-                    : 'text-gray-600 text-lg/8 mt-6'
-                } leading-relaxed`}
+                className={` rounded-xl p-3 ${message.role === 'user'
+                  ? 'bg-gray-100 text-gray-700 p-6 max-w-[70%]'
+                  : 'text-gray-600 text-lg/8 mt-6'
+                  } leading-relaxed`}
               >
                 {message.content}
               </div>
@@ -118,6 +118,12 @@ const ChatInterface: React.FC<ChatInterfaceProps> = ({
             className="absolute right-3 top-1/2 -translate-y-1/2 p-2 text-blue-500 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
           >
             <Send size={20} />
+          </button>
+          <button
+            disabled={isLoading}
+            className="absolute right-10 top-1/2 -translate-y-1/2 p-2 text-blue-500 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            <HiChip size={22} />
           </button>
         </form>
       </div>
