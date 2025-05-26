5c9b1b1b2fbc4be741b450ef659ae7335bba977c
Anand Kumar <anand@thirdai.com>
2025-05-26 10:27:07 +0530

    nits

--- frontend/app/token-classification/jobs/new/page.tsx
+++ frontend/app/token-classification/jobs/new/page.tsx
@@ -56,9 +56,10 @@ const SourceOption: React.FC<SourceOptionProps> = ({
   <div
     className={`relative p-6 border rounded-md transition-all
       ${isSelected ? 'border-blue-500 border-2' : 'border-gray-200 border-2'}
-      ${disabled
-        ? 'opacity-85 cursor-not-allowed bg-gray-50'
-        : 'cursor-pointer hover:border-blue-300'
+      ${
+        disabled
+          ? 'opacity-85 cursor-not-allowed bg-gray-50'
+          : 'cursor-pointer hover:border-blue-300'
       }
     `}
     onClick={() => !disabled && onClick()}
@@ -174,8 +175,8 @@ export default function NewJobPage() {
 
     const fetchingReportName = async () => {
       const response = await nerService.listReports();
-      setExistingReportName(response.map((report) => report.ReportName))
-    }
+      setExistingReportName(response.map((report) => report.ReportName));
+    };
     fetchingReportName();
   }, []);
 
@@ -491,14 +492,14 @@ export default function NewJobPage() {
         CustomTags: customTagsObj,
         ...(selectedSource === 's3'
           ? {
-            S3Endpoint: sourceS3Endpoint,
-            S3Region: sourceS3Region,
-            SourceS3Bucket: sourceS3Bucket,
-            SourceS3Prefix: sourceS3Prefix || undefined,
-          }
+              S3Endpoint: sourceS3Endpoint,
+              S3Region: sourceS3Region,
+              SourceS3Bucket: sourceS3Bucket,
+              SourceS3Prefix: sourceS3Prefix || undefined,
+            }
           : {
-            UploadId: uploadId,
-          }),
+              UploadId: uploadId,
+            }),
         Groups: groups,
         report_name: jobName,
       });
@@ -538,7 +539,7 @@ export default function NewJobPage() {
     }
 
     if (existingReportName.includes(name)) {
-      setNameError("Report with this name already exsits.")
+      setNameError('Report with this name already exists.');
       return false;
     }
 
@@ -632,8 +633,9 @@ export default function NewJobPage() {
                   validateJobName(value);
                 }}
                 onBlur={() => validateJobName(jobName)}
-                className={`w-full p-2 border ${nameError ? 'border-red-500' : 'border-gray-300'
-                  } rounded`}
+                className={`w-full p-2 border ${
+                  nameError ? 'border-red-500' : 'border-gray-300'
+                } rounded`}
                 placeholder="Enter_Report_Name"
                 required
               />
@@ -914,7 +916,7 @@ export default function NewJobPage() {
                     </>
                   }
                   isSelected={false}
-                  onClick={() => { }}
+                  onClick={() => {}}
                   disabled={true}
                 />
               </div>
@@ -1073,8 +1075,9 @@ export default function NewJobPage() {
                         value={customTagName}
                         onChange={(e) => handleTagNameChange(e.target.value)}
                         onBlur={(e) => handleTagNameChange(e.target.value)}
-                        className={`w-full p-2 border ${nameError ? 'border-red-500' : 'border-gray-300'
-                          } rounded`}
+                        className={`w-full p-2 border ${
+                          nameError ? 'border-red-500' : 'border-gray-300'
+                        } rounded`}
                         placeholder="CUSTOM_TAG_NAME"
                         required
                       />
