6a4f49a642862278afe7981030048616d2a1893f
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2025-05-28 15:47:55 -0400

    Fix migrations (#172)

--- internal/database/versions/migration_1/migration_1.go
+++ internal/database/versions/migration_1/migration_1.go
@@ -1,4 +1,4 @@
-package versions
+package migration_1
 
 import (
 	"fmt"
@@ -6,21 +6,25 @@ import (
 	"gorm.io/gorm"
 )
 
-func Migration1(db *gorm.DB) error {
-	if err := db.Migrator().AddColumn(&InferenceTask{}, "CompletedSize"); err != nil {
+type InferenceTask struct {
+	CompletedSize int64 `gorm:"default:0"`
+}
+
+func Migration(db *gorm.DB) error {
+	if err := db.Migrator().AddColumn(&InferenceTask{}, "completed_size"); err != nil {
 		return fmt.Errorf("error adding CompletedSize column: %w", err)
 	}
 
 	if err := db.Model(&InferenceTask{}).
-		Where("CompletedSize IS NULL").
-		Update("CompletedSize", 0).Error; err != nil {
+		Where("completed_size IS NULL").
+		Update("completed_size", 0).Error; err != nil {
 		return fmt.Errorf("error setting default value for CompletedSize: %w", err)
 	}
 
 	return nil
 }
 
-func RollbackMigration1(db *gorm.DB) error {
+func Rollback(db *gorm.DB) error {
 	if err := db.Migrator().DropColumn(&InferenceTask{}, "CompletedSize"); err != nil {
 		return fmt.Errorf("error dropping CompletedSize column: %w", err)
 	}
