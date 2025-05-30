df4543fb9948fbe8813ec228910e8a11bb218017
BenGeordie <school@bengeordie@gmail.com>
2025-05-30 14:02:02 -0500

    Rename electron to electron API, fix s3 error

--- frontend/app/token-classification/jobs/page.tsx
+++ frontend/app/token-classification/jobs/page.tsx
@@ -476,7 +476,7 @@ function JobDetail() {
           <DatabaseTable
             groups={reportData?.Groups?.map((g) => g.Name) || []}
             tags={availableTagsCount}
-            uploadId={reportData?.SourceS3Prefix}
+            uploadId={reportData?.IsUpload ? reportData?.SourceS3Prefix : ''}
           />
         </TabsContent>
       </Tabs>
