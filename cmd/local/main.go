8dc9b9855d3a53e95659e19942b1975847e18875
ashutoshTAI <ashutosh@thirdai.com>
2025-06-04 13:19:26 -0500

    Remove model directory before attempting fresh download. (#205)

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -182,10 +182,14 @@ func main() {
 	}
 
 	boltDir := filepath.Join(cfg.Root, "models", basicModel.Id.String())
+	// If the model directory already exists, then backend is unable to start, so we first remove the
+	// old directory to ensure a fresh download.
+	if err := os.RemoveAll(boltDir); err != nil {
+		log.Fatalf("failed to remove existing model directory: %v", err)
+	}
 	if err := storage.DownloadDir(context.Background(), modelBucket, basicModel.Id.String(), boltDir); err != nil {
 		log.Fatalf("failed to download bolt model: %v", err)
 	}
-
 	server := createServer(db, storage, queue, cfg.Port, boltDir)
 
 	slog.Info("starting worker")
