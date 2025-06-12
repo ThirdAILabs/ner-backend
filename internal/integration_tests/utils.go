9c8323ca68b24df620c767d5182cfc4763fa0e18
Nicholas Meisburger <43160219+nmeisburger@users.noreply.github.com>
2025-06-12 11:03:34 -0400

    ONNX model finetuning (#232)

--- internal/integration_tests/utils.go
+++ internal/integration_tests/utils.go
@@ -57,13 +57,17 @@ func (m *regexModel) Predict(text string) ([]types.Entity, error) {
 	return entities, nil
 }
 
-func (m *regexModel) Finetune(taskPrompt string, tags []api.TagInfo, samples []api.Sample) error {
+func (m *regexModel) FinetuneAndSave(taskPrompt string, tags []api.TagInfo, samples []api.Sample, savePath string) error {
 	for _, tag := range tags {
 		pattern, err := regexp.Compile(tag.Name)
 		if err != nil {
 			return fmt.Errorf("error compiling regex pattern: %w", err)
 		}
 		m.patterns[tag.Name] = *pattern
+	}
+
+	if err := m.Save(savePath); err != nil {
+		return fmt.Errorf("error saving model: %w", err)
 	}
 
 	return nil
