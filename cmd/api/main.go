750e58ad525f640f802ed8ad90f857939473296b
BenGeordie <school@bengeordie@gmail.com>
2025-06-26 14:18:03 -0500

    Rename APIConfig env variable names

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,9 +24,9 @@ import (
 type APIConfig struct {
 	DatabaseURL       string `env:"DATABASE_URL,notEmpty,required"`
 	RabbitMQURL       string `env:"RABBITMQ_URL,notEmpty,required"`
-	Endpoint     string `env:"S3_ENDPOINT_URL,notEmpty,required"`
-	AccessKeyID     string `env:"INTERNAL_AWS_ACCESS_KEY_ID,notEmpty,required"`
-	SecretAccessKey string `env:"INTERNAL_AWS_SECRET_ACCESS_KEY,notEmpty,required"`
+	S3EndpointURL     string `env:"S3_ENDPOINT_URL,notEmpty,required"`
+	S3AccessKeyID     string `env:"INTERNAL_AWS_ACCESS_KEY_ID,notEmpty,required"`
+	S3SecretAccessKey string `env:"INTERNAL_AWS_SECRET_ACCESS_KEY,notEmpty,required"`
 	ModelBucketName   string `env:"MODEL_BUCKET_NAME" envDefault:"ner-models"`
 	QueueNames        string `env:"QUEUE_NAMES" envDefault:"inference_queue,training_queue,shard_data_queue"`
 	WorkerConcurrency int    `env:"CONCURRENCY" envDefault:"1"`
@@ -52,9 +52,9 @@ func main() {
 	}
 
 	s3Cfg := storage.S3ClientConfig{
-		Endpoint:     cfg.Endpoint,
-		AccessKeyID:     cfg.AccessKeyID,
-		SecretAccessKey: cfg.SecretAccessKey,
+		Endpoint:     cfg.S3EndpointURL,
+		AccessKeyID:     cfg.S3AccessKeyID,
+		SecretAccessKey: cfg.S3SecretAccessKey,
 	}
 	s3ObjectStore, err := storage.NewS3ObjectStore(s3Cfg)
 	if err != nil {
