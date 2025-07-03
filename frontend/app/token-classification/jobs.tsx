09d26f759cfad1551e8bc689c430580f8790215d
Anand Kumar <anand@thirdai.com>
2025-07-03 20:29:44 +0530

    fixed licence bug

--- frontend/app/token-classification/jobs.tsx
+++ frontend/app/token-classification/jobs.tsx
@@ -402,7 +402,7 @@ export default function Jobs() {
   const [error, setError] = useState<string | null>(null);
   const { healthStatus } = useHealth();
   const { license } = useLicense();
-  const [isLicenseValid, setIsLicenseValid] = useState<boolean | null>(true);
+  const [isLicenseValid, setIsLicenseValid] = useState<boolean>(true);
 
   useEffect(() => {
     recordEvent({
@@ -411,13 +411,14 @@ export default function Jobs() {
       Page: 'Reports Dashboard Page',
     });
   }, []);
-
+  console.log('license', license, 'isLicenseValid', isLicenseValid);
   useEffect(() => {
-    setIsLicenseValid(
-      license &&
-        license.LicenseError !== 'expired license' &&
-        license.LicenseError !== 'invalid license'
-    );
+    let licenceValidityCheck = true;
+    if (license && license.LicenseError) {
+      licenceValidityCheck =
+        license.LicenseError !== 'expired license' && license.LicenseError !== 'invalid license';
+    }
+    setIsLicenseValid(licenceValidityCheck);
   }, [license]);
 
   const pollingIntervalRef = useRef<NodeJS.Timeout | null>(null);
