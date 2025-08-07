c6a57a06f7a71f37c395db21180fc218ff18e8f3
BenGeordie <school@bengeordie@gmail.com>
2025-08-07 16:21:14 -0500

    Copy licenses

--- electron-ner-app/scripts/copy-licenses.js
+++ electron-ner-app/scripts/copy-licenses.js
@@ -8,51 +8,38 @@ const __dirname = path.dirname(__filename);
 
 // Paths
 const projectRoot = path.join(__dirname, '..');
-const projectLicense = path.join(projectRoot, 'THIRD_PARTY_NOTICES.txt');
-const frontendRoot = path.join(__dirname, '../../frontend');
-const frontendLicense = path.join(frontendRoot, 'THIRD_PARTY_NOTICES.txt');
-const licenseDir = path.join(projectRoot, 'licenses');
+const targetLicenseDir = path.join(projectRoot, 'licenses');
+
+const licenseMetadata = [
+  {name: 'ELECTRON', dir: './'},
+  {name: 'FRONTEND', dir: '../../frontend'},
+  {name: 'PYTHON_PLUGIN', dir: '../../plugin/plugin-python'},
+  {name: 'BOLT', dir: '../../internal/core/bolt/lib'},
+]
 
 // Ensure license directory exists
-if (!fs.existsSync(licenseDir)) {
+if (!fs.existsSync(targetLicenseDir)) {
   console.log('Creating license directory...');
-  fs.mkdirSync(licenseDir, { recursive: true });
+  fs.mkdirSync(targetLicenseDir, { recursive: true });
 }
 
-// Copy project license
-if (!fs.existsSync(projectLicense)) {
-  console.error('Project license not found at:', projectLicense);
-  process.exit(1);
-}
-
-// Copy frontend license
-if (!fs.existsSync(frontendLicense)) {
-  console.error('Frontend license not found at:', frontendLicense);
-  process.exit(1);
-}
-
-// Copy project license
 try {
-  const targetProjectLicense = path.join(licenseDir, 'THIRD_PARTY_NOTICES.txt');
+  // Copy licenses
+  for (const license of licenseMetadata) {
+    const licensePath = path.join(projectRoot, license.dir, 'THIRD_PARTY_NOTICES.txt');
+    if (!fs.existsSync(licensePath)) {
+      console.error(`${license.name} license not found at: ${licensePath}`);
+      process.exit(1);
+    }
 
-  fs.cpSync(projectLicense, targetProjectLicense, {
-    recursive: true,
-    force: true,
-    dereference: false,
-    preserveTimestamps: true
-  });
-
-  // Read frontend license content
-  const frontendLicenseContent = fs.readFileSync(frontendLicense, 'utf8');
-  
-  // Replace first two lines with dashes
-  const modifiedContent = frontendLicenseContent.split('\n')
-    .slice(2)
-    .join('\n');
-  
-  // Append modified content to project license
-  fs.appendFileSync(targetProjectLicense, '\n\n-----------\n' + modifiedContent);
-
+    const targetLicensePath = path.join(targetLicenseDir, `${license.name}_THIRD_PARTY_NOTICES.txt`);
+    fs.cpSync(licensePath, targetLicensePath, {
+      recursive: true,
+      force: true,
+      dereference: false,
+      preserveTimestamps: true
+    });
+  }
   console.log('Licenses copied successfully!');
 } catch (error) {
   console.error('Failed to copy licenses:', error.message);
